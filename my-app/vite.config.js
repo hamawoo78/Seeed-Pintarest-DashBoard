f30657a473de144a780acad9aa05a038e2baebd4
Nae <nae.yk.jp@gmail.com>
2025-11-06 23:54:23 -0800

    adding Privacy policy

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,5 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'), // 👈 add this line
+      },
+    },
+  },
+  base: '/Seeed-Pintarest-DashBoard/', // 👈 important for GitHub Pages
   
 })
